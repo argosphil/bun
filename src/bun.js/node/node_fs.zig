21862fdaf87d4b3a77d07710790fcf76ac7428cf
Philip Russell <argosphil@murena.io>
2024-03-24 10:31:11 +0000

    explicitly NUL-terminate buffer

--- src/bun.js/node/node_fs.zig
+++ src/bun.js/node/node_fs.zig
@@ -5272,6 +5272,8 @@ pub const NodeFS = struct {
         }
 
         buf.items.len = if (comptime string_type == .null_terminated) total + 1 else total;
+	if (comptime string_type == .null_terminated)
+            buf.items.ptr[total] = '\0';
         if (total == 0) {
             buf.deinit();
             return switch (args.encoding) {
